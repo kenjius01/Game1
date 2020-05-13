4a0ab7c873f3e89741b3ab85b213d363299254db
kenjius01 <63000038+kenjius01@users.noreply.github.com>
2020-05-13 09:28:18 +0700

    Update window.cpp

--- window.cpp
+++ window.cpp
@@ -1,7 +1,6 @@
 #include "window.h"
 #include<SDL_image.h>
-//*****************************************************
-// Các hàm chung về khởi tạo và huỷ SDL
+
 void logSDLError(std::ostream& os,
                  const std::string &msg, bool fatal)
 {
@@ -20,13 +19,9 @@ void initSDL(SDL_Window* &window, SDL_Renderer* &renderer,
 
     window = SDL_CreateWindow(windowTitle, SDL_WINDOWPOS_CENTERED,
        SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
-    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
-    //   SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_FULLSCREEN_DESKTOP);
+   
     if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
 
-    //Khi thông thường chạy với môi trường bình thường ở nhà
-    //renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    //Khi chạy ở máy thực hành WinXP ở trường (máy ảo)
     renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
     if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
 
@@ -55,15 +50,14 @@ void waitUntilKeyPressed()
 
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
 {
-	//Khởi tạo là nullptr để tránh lỗi 'dangling pointer'
 	SDL_Texture *texture = nullptr;
-	//Nạp ảnh từ tên file (với đường dẫn)
+	
 	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
-	//Nếu không có lỗi, chuyển đổi về dạng texture and và trả về
+	
 	if (loadedImage != nullptr){
 		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
 		SDL_FreeSurface(loadedImage);
-		//Đảm bảo việc chuyển đổi không có lỗi
+		
 		if (texture == nullptr){
 			logSDLError(std::cout, "CreateTextureFromSurface");
 		}
@@ -91,17 +85,6 @@ SDL_Texture* loadTexturepng(const std::string &file, SDL_Renderer *ren)
 	return texture;
 }
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int w, int h)
-{
-	//Thiết lập hình chữ nhật đích mà chúng ta muốn vẽ ảnh vào trong
-	SDL_Rect dst;
-	dst.w = w;
-	dst.h = h;
-	//Truy vẫn texture để lấy chiều rộng và cao (vào chiều rộng và cao tương ứng của hình chữ nhật đích)
-	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
-    //Đưa toàn bộ ảnh trong texture vào hình chữ nhật đích
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
 
 
 
@@ -111,9 +94,8 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int
 	SDL_Rect dst;
 	dst.x = x;
 	dst.y = y;
-    dst.w = w;
-    dst.h = h;
-    //Đưa toàn bộ ảnh trong texture vào hình chữ nhật đích
-    //(ảnh sẽ co dãn cho khớp với kích cỡ mới)
+        dst.w = w;
+        dst.h = h;
+   
 	SDL_RenderCopy(ren, tex, NULL, &dst);
 }
